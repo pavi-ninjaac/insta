11c691bebf789dc0ab0c754f27d3812e6bba43c9
PaviNinjac <51699297+pavi-ninjaac@users.noreply.github.com>
2020-04-30 22:59:50 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,21 @@
+c,d="hello","hello"
+e,f=[1,2],[1,2]
+g,h=[1,2],[1,2,]
+print(c is d,end=".")
+print(e is f,end=".")
+print(g is h,end="")
+_________________
+Find the o/p?
+a)True.True.False
+b)True.False.False
+c)True.True.True
+d)Error
+
+
+
+
+
+
+
+
+
